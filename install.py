a48493fa984d9f4d3c6d3e784b2027e7130e4253
Britt Gresham <britt@puppetlabs.com>
2015-02-16 21:01:15 -0800

    Remove useless comments and PEP-8 refactor

--- install.py
+++ install.py
@@ -6,36 +6,10 @@ import re
 import commands
 
 
-# VIM - Vi IMproved 7.3 (2010 Aug 15, compiled Aug 24 2013 18:58:47)
-# Compiled by root@apple.com
-# Normal version without GUI.  Features included (+) or not (-):
-# -arabic +autocmd -balloon_eval -browse +builtin_terms +byte_offset +cindent
-# -clientserver -clipboard +cmdline_compl +cmdline_hist +cmdline_info +comments
-# -conceal +cryptv +cscope +cursorbind +cursorshape +dialog_con +diff +digraphs
-# -dnd -ebcdic -emacs_tags +eval +ex_extra +extra_search -farsi +file_in_path
-# +find_in_path +float +folding -footer +fork() -gettext -hangul_input +iconv
-# +insert_expand +jumplist -keymap -langmap +libcall +linebreak +lispindent
-# +listcmds +localmap -lua +menu +mksession +modify_fname +mouse -mouseshape
-# -mouse_dec -mouse_gpm -mouse_jsbterm -mouse_netterm -mouse_sysmouse
-# +mouse_xterm +multi_byte +multi_lang -mzscheme +netbeans_intg -osfiletype
-# +path_extra -perl +persistent_undo +postscript +printer -profile +python/dyn
-# -python3 +quickfix +reltime -rightleft +ruby/dyn +scrollbind +signs
-# +smartindent -sniff +startuptime +statusline -sun_workshop +syntax +tag_binary
-# +tag_old_static -tag_any_white -tcl +terminfo +termresponse +textobjects +title
-#  -toolbar +user_commands +vertsplit +virtualedit +visual +visualextra +viminfo
-#  +vreplace +wildignore +wildmenu +windows +writebackup -X11 -xfontset -xim -xsmp
-# -xterm_clipboard -xterm_save
-# system vimrc file: "$VIM/vimrc"
-# user vimrc file: "$HOME/.vimrc"
-# user exrc file: "$HOME/.exrc"
-# fall-back for $VIM: "/usr/share/vim"
-# Compilation: gcc -c -I. -D_FORTIFY_SOURCE=0 -Iproto -DHAVE_CONFIG_H -arch i386 -arch x86_64 -g -Os -pipe
-# fall-back for $VIM: "/usr/share/vim"
-
-
 def get_vimrc_locations():
     version_info = commands.getoutput("vim --version")
     return [os.path.expandvars(x) for x in re.findall(r'"(.*[_\.]g?vimrc)"', version_info)]
+
 
 def get_git_repo(bundle_str):
     parts = bundle_str.split("/")
@@ -46,7 +20,6 @@ def get_git_repo(bundle_str):
 
 
 def main():
-
     # Find Bundles
     bundles = []
     for vimrc_file in get_vimrc_locations():
